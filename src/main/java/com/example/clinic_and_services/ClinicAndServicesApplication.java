a0a3faf7fac14747a1c7691194da401d02faff93
eduardt <eduard.t@air-dr.com>
2021-02-24 16:21:38 +0200

    Added Spring boot

---
+++ src/main/java/com/example/clinic_and_services/ClinicAndServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinic_and_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAndServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAndServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clinic_and_services/ClinicAndServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clinic_and_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAndServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
